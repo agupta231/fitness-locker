848f6810b063769d8b423516eb85675bede313fa
Ankur Gupta <rukna1000@gmail.com>
2019-03-28 10:01:51 -0400

    Create more endpoints

--- src/locker.py
+++ src/locker.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import json
+import gym
 
 app = Flask(__name__)
 
@@ -6,6 +8,17 @@ app = Flask(__name__)
 def root():
     print("Hello, World!")
     return "Hello World!"
+
+@app.route("/smart_locker/gym", methods=['GET'])
+def register_gym_trigger():
+    pass
+
+@app.route("/smart_locker/gym/debug", methods=['GET'])
+def gym_debug():
+    var_dump = {}
+    var_dump['last_access'] = gym.last_trigger_time
+
+    return json.dumps(var_dump)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
