1c9bb12f926f822893d38cf3ce04654fc5a91c64
Ankur Gupta <rukna1000@gmail.com>
2019-03-28 09:39:59 -0400

    Create basic flask app

--- src/locker.py
+++ src/locker.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    print("Hello, World!")
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
